7d193a9ea33f11099acdf6f8c0eaec9406e87235
ruizdev98 <rruiz.dev98@gmail.com>
2026-02-18 18:32:41 -0500

    adicione logica en MainLayout.jsx 9

--- src/app/layout/MainLayout.jsx
+++ src/app/layout/MainLayout.jsx
@@ -1,31 +1,10 @@
 // src/app/layout/MainLayout.jsx
-import { useEffect } from "react"
 import { Outlet } from 'react-router-dom'
-import { useCartContext } from "@/core/context/CartContext"
-import api from "@/core/api/api"
 import Header from '@/components/header/Header'
 import Footer from '@/components/footer/Footer'
 
 
 const MainLayout = () => {
-  const { clearCart } = useCartContext()
-
-  useEffect(() => {
-    const checkPaidOrder = async () => {
-      try {
-        const { data } = await api.get("/orders/pending")
-
-        if (data?.status === "paid") {
-          clearCart()
-        }
-      } catch (err) {
-        console.error(err)
-      }
-    }
-
-    checkPaidOrder()
-  }, [])
-
   return (
     <>
       <Header />
