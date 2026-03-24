85dfae850d43486c138fc97853311dd6f5f9fb3d
ruizdev98 <rruiz.dev98@gmail.com>
2026-03-24 12:00:49 -0500

    Fix CategoryPage.jsx file version5

--- src/domains/categories/pages/CategoryPage.jsx
+++ src/domains/categories/pages/CategoryPage.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import ProductSection from "@/domains/products/section/ProductSection"
 import styles from "./CategoryPage.module.css"
 
