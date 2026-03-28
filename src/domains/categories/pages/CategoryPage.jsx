f363fbead6208a39afbc66ada996aaa168bc6977
ruizdev98 <rruiz.dev98@gmail.com>
2026-03-28 13:18:30 -0500

    Fix CategoryPage.module.css and CategoryPage.jsx file version3

--- src/domains/categories/pages/CategoryPage.jsx
+++ src/domains/categories/pages/CategoryPage.jsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import { capitalizeFirstLetter } from "@/core/utils/textFormat"
-import api from "@/core/api/api"
-import ProductCard from "@/domains/products/card/ProductCard"
-import InputField from "@/shared/ui/InputField"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSliders } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { capitalizeFirstLetter } from '@/core/utils/textFormat'
+import api from '@/core/api/api'
+import ProductCard from '@/domains/products/card/ProductCard'
+import InputField from '@/shared/ui/InputField'
 import GeneralButton from '@/shared/ui/GeneralButton'
 import styles from "./CategoryPage.module.css"
 
@@ -109,71 +111,76 @@ export default function CategoryPage() {
       
       {/* 🔥 SIDEBAR */}
       <div className={styles.category}>
-        <aside className={styles.filters}>
-          <h2 className={styles.categoryName}>{categoryName}</h2>
-          <h3>Filtros</h3>
-
-          {/* MARCAS */}
-          <div className={styles.filterBlock}>
-            <p className={styles.filterTitle}>Marca</p>
-
-            {brands.map(brand => (
-              <label key={brand} className={styles.checkbox}>
-                <input
-                  type="checkbox"
-                  checked={selectedBrands.includes(brand)}
-                  onChange={() => toggleBrand(brand)}
-                />
-                <span className={styles.customCheckbox}></span>
-                <span>{brand}</span>
-              </label>
-            ))}
+        <aside className={styles.filtersContainer}>
+          <div className={styles.titleContainer}>
+            <h2 className={styles.categoryTitle}>{categoryName}</h2>
           </div>
+          
+          <div className={styles.filters}>
+            <h3 className={styles.filterTitle}><FontAwesomeIcon icon={faSliders} />Filtros</h3>
+            {/* MARCAS */}
+            <div className={styles.filterBlock}>
+              <p className={styles.filterTitle}>Marca</p>
 
-          {/* PRECIO */}
-          <div className={styles.filterBlock}>
-            <p className={styles.filterTitle}>Precio</p>
-            <div className={styles.inputs}>
-              <InputField
-                name='numberMin'
-                type="number"
-                placeholder="Min"
-                value={priceRange.min}
-                onChange={(e) => setPriceRange({ ...priceRange, min: e.target.value })}
-              />
-
-              <InputField
-                name='numberMax'
-                type="number"
-                placeholder="Max"
-                value={priceRange.max}
-                onChange={(e) => setPriceRange({ ...priceRange, max: e.target.value })}
-              />
+              {brands.map(brand => (
+                <label key={brand} className={styles.checkbox}>
+                  <input
+                    type="checkbox"
+                    checked={selectedBrands.includes(brand)}
+                    onChange={() => toggleBrand(brand)}
+                  />
+                  <span className={styles.customCheckbox}></span>
+                  <span>{brand}</span>
+                </label>
+              ))}
             </div>
-            
+
+            {/* PRECIO */}
+            <div className={styles.filterBlock}>
+              <p className={styles.filterTitle}>Precio</p>
+              <div className={styles.inputs}>
+                <InputField
+                  name='numberMin'
+                  type="number"
+                  placeholder="Min"
+                  value={priceRange.min}
+                  onChange={(e) => setPriceRange({ ...priceRange, min: e.target.value })}
+                />
+
+                <InputField
+                  name='numberMax'
+                  type="number"
+                  placeholder="Max"
+                  value={priceRange.max}
+                  onChange={(e) => setPriceRange({ ...priceRange, max: e.target.value })}
+                />
+              </div>
+              
+            </div>
+
+            {/* 🔥 BOTONES */}
+            <div className={styles.filterActions}>
+              <GeneralButton
+                size='medium'
+                className={styles.applyBtn}
+                onClick={applyFilters}
+                disabled={!hasFilters}
+              >
+                Aplicar filtros
+              </GeneralButton>
+
+              <GeneralButton
+                variant='secondary'
+                size='medium'
+                className={styles.clearBtn}
+                onClick={clearFilters}
+                disabled={!hasFilters}
+              >
+                Limpiar
+              </GeneralButton>
+            </div>
           </div>
 
-          {/* 🔥 BOTONES */}
-          <div className={styles.filterActions}>
-            <GeneralButton
-              size='medium'
-              className={styles.applyBtn}
-              onClick={applyFilters}
-              disabled={!hasFilters}
-            >
-              Aplicar filtros
-            </GeneralButton>
-
-            <GeneralButton
-              variant='secondary'
-              size='medium'
-              className={styles.clearBtn}
-              onClick={clearFilters}
-              disabled={!hasFilters}
-            >
-              Limpiar
-            </GeneralButton>
-          </div>
         </aside>
 
         {/* PRODUCTOS */}
