32f4bd84916fdb18121353fbe211d4ba8f14242f
ruizdev98 <rruiz.dev98@gmail.com>
2026-04-01 17:29:56 -0500

    Fix router.jsx version5

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -17,7 +17,7 @@ import PaymentPage from "@/domains/checkout/payment/PaymentPage"
 // Páginas por dominio
 import CategoryPage from "@/domains/categories/pages/CategoryPage"
 import ProductDetailPage from "@/domains/products/pages/ProductDetailPage"
-import BestSellerPage from '@/domains/products/pages/BestSellersPage'
+import BestSellersPage from '@/domains/products/pages/BestSellersPage'
 
 // Página no encontrada
 import NotFoundPage from "@/pages/NotFoundPage"
