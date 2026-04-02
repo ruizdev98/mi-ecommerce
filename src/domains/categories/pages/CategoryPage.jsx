ca0c710e1b4d049c815b89b24ed30406654b21d6
ruizdev98 <rruiz.dev98@gmail.com>
2026-04-02 10:31:28 -0500

    Refactor all version3

--- src/domains/categories/pages/CategoryPage.jsx
+++ src/domains/categories/pages/CategoryPage.jsx
@@ -6,7 +6,7 @@ import useProductsFilters from '@/domains/categories/hooks/useProductsFilters'
 import useIsMobile from '@/core/hooks/useIsMobile'
 import ProductCard from '@/domains/products/card/ProductCard'
 import GeneralButton from '@/shared/ui/GeneralButton'
-import FiltersPanel from './FiltersPanel'
+//import FiltersPanel from './FiltersPanel'
 import styles from "./CategoryPage.module.css"
 
 export default function CategoryPage() {
@@ -41,29 +41,7 @@ export default function CategoryPage() {
   return (
     <div className={`container`}>
       <div className={styles.category}>
-        {isMobile ? (
-          <>
-            <GeneralButton
-              size='medium'
-              className={styles.showFilters}
-              onClick={() => setIsSidebarOpen(true)}
-            >
-              <FontAwesomeIcon icon={faSliders} />Filtrar
-            </GeneralButton>
-
-            {/* Overlay */}
-            <div 
-              className={`${styles.overlay} ${isSidebarOpen ? styles.show : ''}`}
-              onClick={() => setIsSidebarOpen(false)}
-            />
-            {/* Sidebar */}
-            <div className={`${styles.mobileSidebar} ${isSidebarOpen ? styles.open : ''}`}>
-              <FiltersPanel {...filtersProps} />
-            </div>
-          </>
-        ) : (
-          <FiltersPanel {...filtersProps} />
-        )}
+        
 
         {/* PRODUCTOS */}
         <div className={styles.productsContainer}>
