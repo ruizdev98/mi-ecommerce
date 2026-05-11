d6b31949f20ac690c9301a5524e29e80c3b4e228
ruizdev98 <rruiz.dev98@gmail.com>
2026-05-11 11:20:20 -0500

    Fix BrandsSection.module.css file

--- src/domains/brands/section/BrandsSection.jsx
+++ src/domains/brands/section/BrandsSection.jsx
@@ -5,17 +5,20 @@ export default function BrandsSection({title, products, brands}) {
     const usedBrandIds = new Set(products.map(p => p.brandId));
     const featuredBrands = brands.filter(brand => usedBrandIds.has(brand.id)).slice(0, 8);
   return (
-    <section className={`container ${styles.brandsSection}`}>
-        <SectionTitle title={title} />
-        <div className={styles.grid}>
-            {featuredBrands.map(brand => (
-            <div key={brand.id} className={styles.item}>
-                <a href="#" className={styles.link}>
-                    <img src={brand.logo} alt={brand.name} className={styles.logo} />
-                </a>
+    <section className={`container`}>
+        <div className={styles.brandsSection}>
+            <SectionTitle title={title} />
+            <div className={styles.grid}>
+                {featuredBrands.map(brand => (
+                <div key={brand.id} className={styles.item}>
+                    <a href="#" className={styles.link}>
+                        <img src={brand.logo} alt={brand.name} className={styles.logo} />
+                    </a>
+                </div>
+                ))}
             </div>
-            ))}
         </div>
+        
     </section>
   )
 }
